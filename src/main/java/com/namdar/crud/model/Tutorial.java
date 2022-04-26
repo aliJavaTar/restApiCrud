2058018c9a35ef278c17687e4a5f62b76b7e5359
alijavaboy <ali77erfagh99@gmail.com>
2022-04-26 13:19:00 +0430

    class tutorial add

---
+++ src/main/java/com/namdar/crud/model/Tutorial.java
@@ -0,0 +1,33 @@
+package com.namdar.crud.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = Tutorial.TABLE_NAME)
+public class Tutorial {
+    protected static final String TABLE_NAME = "tutorials";
+    private static final String TITLE = "title";
+    private static final String DESCRIPTION = "description";
+    private static final String PUBLISHED = "published";
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotBlank
+    @Column(name = TITLE, nullable = false)
+    private String title;
+    @Column(name = DESCRIPTION)
+    private String description;
+    @Column(name = PUBLISHED)
+    private boolean published;
+}
